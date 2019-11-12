8030646b5038f442a28b71c86f38c5f18abb192e
Kevin Shewey <Kevin.Shewey@gmail.com>
2019-11-11 23:17:00 -0800

    added upload avatar image feature using multer and sharp npm packages

--- src/index.js
+++ src/index.js
@@ -6,39 +6,10 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// middleware
-// app.use((req, res, next) => {
-//     if(req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
-
-// app.use((req, res, next) => {
-//     res.status(503).send('The site is under maintenece')
-// })
-
-
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
-
-
-
 app.listen(port, () => {
     console.log('Server is up on port' + port)
 })
-
-const Task = require('./models/task')
-const User = require('./models/user')
-
-// const main = async () => {
-//     const user = await User.findById('5dc0f6a17bf317624a71027c')
-//     await user.populate('tasks').execPopulate()
-//     console.log(user.tasks)
-// }
-
-// main()
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth.js')
 const router = new express.Router()
+
 
 
 
@@ -112,4 +115,50 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 })
 
+//endpoint to upload an avatar
+const upload = multer({
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req,file, cb) {
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image file'))
+
+        }
+        cb(undefined, true)
+    }
+})
+
+//endpoint to upload a user avatar
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => {
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message})
+}) 
+
+//endpoint to delete a user avatar
+
+router.delete('/users/me/avatar', auth, async (req, res) => {
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+
+        if(!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-Type','image/png')
+        res.send(user.avatar)
+    } catch(error) {
+        res.status(404).send()
+    }
+})
 module.exports = router
